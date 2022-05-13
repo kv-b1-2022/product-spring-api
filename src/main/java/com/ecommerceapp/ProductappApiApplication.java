d0cfdb332ed4bb4d9f8b1c2299575a29f92b2364
Naresh <nareshkumarh@live.com>
2022-05-13 11:03:03 +0530

    Added demo project

---
+++ src/main/java/com/ecommerceapp/ProductappApiApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductappApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductappApiApplication.class, args);
+	}
+
+}
