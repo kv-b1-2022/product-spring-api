50759f6d795b7ca91bc634c451785d3b562d658d
manojkumar9908 <manojroyal9908@gmail.com>
2022-05-13 19:29:50 +0530

    updated src

---
+++ src/main/java/com/ecommerceapp/productprice/validation/PriceValidation.java
@@ -0,0 +1,35 @@
+package com.ecommerceapp.productprice.validation;
+
+import java.time.LocalDate;
+
+import com.ecommerceapp.productprice.model.ProductPrice;
+
+
+
+public class PriceValidation {
+
+	public static void validate(ProductPrice productprice) throws Exception {
+		System.out.println(productprice);
+		if (productprice.getProductId()<=0) {
+			throw new Exception("Invalid Id. Id cannot be empty/blank");
+		}
+		if (productprice.getProductId()<=0 ) {
+			throw new Exception("Invalid ProductId. 	ProductId cannot be empty/blank");
+		}
+		if (productprice.getPrice() == null || productprice.getPrice() <= 0) {
+			throw new Exception("Price Must not be null , Price must be greater than 0");
+		}
+         if( productprice.getStartDate() == null) {
+        	 throw new Exception(" Date cannot be null ");
+        	 
+         }
+      
+		if (productprice.getStartDate().isBefore(LocalDate.now())) {
+			throw new Exception("Start date cannot be less than current date");
+		}
+//		if (productprice.getEndDate() == null || productprice.getEndDate().equals("")) {
+//			throw new Exception("Invalid StartDate. StartDate cannot be empty/blank1");
+//		}
+	}
+
+}
