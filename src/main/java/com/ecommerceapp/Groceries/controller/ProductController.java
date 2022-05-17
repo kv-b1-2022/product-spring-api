295a38983135579ba801d09a8da1804c456681cc
Naresh <nareshkumarh@live.com>
2022-05-17 17:10:01 +0530

    added cors

--- src/main/java/com/ecommerceapp/Groceries/controller/ProductController.java
+++ src/main/java/com/ecommerceapp/Groceries/controller/ProductController.java
@@ -6,7 +6,8 @@ package com.ecommerceapp.Groceries.controller;
 	import org.springframework.beans.factory.annotation.Autowired;
 	import org.springframework.http.HttpStatus;
 	import org.springframework.http.ResponseEntity;
-	import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 	import org.springframework.web.bind.annotation.GetMapping;
 	import org.springframework.web.bind.annotation.PathVariable;
 	import org.springframework.web.bind.annotation.PostMapping;
@@ -39,6 +40,7 @@ import com.ecommerceapp.Groceries.service.ProductService;
 			}
 		}
 
+		@CrossOrigin
 		@GetMapping("product/productlist")
 		public List<Product> findAll() {
 			List<Product> listProduct = null;
