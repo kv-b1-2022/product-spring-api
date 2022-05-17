5a1e095c450cf9c16ba7bbe628113fab2aaf6999
9361135595Selv@ <selvasl333@gmail.com>
2022-05-17 15:39:21 +0530

    project formated

--- src/main/java/com/ecommerceapp/hotelapp/model/Menus.java
+++ src/main/java/com/ecommerceapp/hotelapp/model/Menus.java
@@ -1,7 +1,5 @@
 package com.ecommerceapp.hotelapp.model;
 
-
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,29 +10,28 @@ import javax.persistence.Table;
 import lombok.Data;
 import lombok.ToString;
 
+@Entity
+@ToString
+@Table(name = "menu_list")
+@Data
+public class Menus {
 
-	@Entity
-	@ToString
-	@Table(name = "menu_list")
-	@Data
-	public class Menus {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
-		private Integer id;
-		
-		@Column(name = "food_name", nullable = false, unique = true, length = 50)
-		private String foodName;
-		
-		@Column(name = "price", nullable = false)
-		private int price;
+	@Column(name = "food_name", nullable = false, unique = true, length = 50)
+	private String foodName;
 
-		@Column(name = "food_type")
-		private String foodType;
-		
-		@Column(name = "style_of_items")
-		private String foodStyle;
-		@Column(name="image_url")
-		private String foodImages;
+	@Column(name = "price", nullable = false)
+	private int price;
+
+	@Column(name = "food_type")
+	private String foodType;
+
+	@Column(name = "style_of_items")
+	private String foodStyle;
+	@Column(name = "image_url")
+	private String foodImages;
 
 }
