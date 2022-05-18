2447f7604fd2ef2a985246a2c0e0aaf64c8a2f03
kishore <you@example.comgit>
2022-05-18 14:03:26 +0530

    added cors filter

--- src/main/java/com/ecommerceapp/ProductappApiApplication.java
+++ src/main/java/com/ecommerceapp/ProductappApiApplication.java
@@ -2,6 +2,9 @@ package com.ecommerceapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProductappApiApplication {
@@ -10,5 +13,17 @@ public class ProductappApiApplication {
 		SpringApplication.run(ProductappApiApplication.class, args);
 		System.out.println("running..");
 	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+	 return new WebMvcConfigurer() {
+	   @Override
+	   
+	   public void addCorsMappings(CorsRegistry registry) {
+	     registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+	   }
+	 };
+	}
+
 
 }
